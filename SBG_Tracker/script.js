f0d359b2f467fe051583fe8e09058713949aeb6d
Peter <99065921@mydavinci.nl>
2021-07-15 01:26:12 +0200

    Raise and decrease resources

--- SBG_Tracker/script.js
+++ SBG_Tracker/script.js
@@ -8,3 +8,51 @@ function statBlock(id){
         target.style.display = "none";
     }
 }
+
+function lowerMight(id){
+    var target = document.getElementById("might"+id); 
+    var newContent = parseInt(target.innerHTML) - 1;
+    target.innerHTML = newContent;
+}
+
+function upMight(id){
+    var target = document.getElementById("might"+id); 
+    var newContent = parseInt(target.innerHTML) + 1;
+    target.innerHTML = newContent;
+}
+
+function lowerWill(id){
+    var target = document.getElementById("will"+id); 
+    var newContent = parseInt(target.innerHTML) - 1;
+    target.innerHTML = newContent;
+}
+
+function upWill(id){
+    var target = document.getElementById("will"+id); 
+    var newContent = parseInt(target.innerHTML) + 1;
+    target.innerHTML = newContent;
+}
+
+function lowerFate(id){
+    var target = document.getElementById("fate"+id); 
+    var newContent = parseInt(target.innerHTML) - 1;
+    target.innerHTML = newContent;
+}
+
+function upFate(id){
+    var target = document.getElementById("fate"+id); 
+    var newContent = parseInt(target.innerHTML) + 1;
+    target.innerHTML = newContent;
+}
+
+function lowerWound(id){
+    var target = document.getElementById("wound"+id); 
+    var newContent = parseInt(target.innerHTML) - 1;
+    target.innerHTML = newContent;
+}
+
+function upWound(id){
+    var target = document.getElementById("wound"+id); 
+    var newContent = parseInt(target.innerHTML) + 1;
+    target.innerHTML = newContent;
+}
