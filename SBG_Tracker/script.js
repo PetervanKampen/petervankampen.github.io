24aad95e9ed2cec5bed389251d8d3001b578c05c
Peter <99065921@mydavinci.nl>
2021-08-22 19:59:32 +0200

    Refresh conformation

--- SBG_Tracker/script.js
+++ SBG_Tracker/script.js
@@ -1,3 +1,8 @@
+window.onbeforeunload = function(event)
+{
+    return confirm("Confirm refresh");
+};
+
 window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13){if(e.target.nodeName=='INPUT'&&e.target.type=='text'){e.preventDefault();return false;}}},true);
 
 function statBlock(id){
