2889ce3b74df2430c704073f0942dcb588357c4c
Peter <99065921@mydavinci.nl>
2022-09-18 13:22:08 +0200

    Logic fully functional. Only needs more data in json file now

--- Scenario_Selector/script.js
+++ Scenario_Selector/script.js
@@ -24,8 +24,16 @@ const generate = () => {
             scenarioResultAll.forEach(singleScenario => {
 
                 singleScenario.classList.remove('hidden')
-                
+
                 const title = singleScenario.querySelector('.title')
+                const outline = singleScenario.querySelector('.outline p')
+                const layout = singleScenario.querySelector('.layout p')
+                const starting_position = singleScenario.querySelector('.starting_position p')
+                const priority = singleScenario.querySelector('.priority p')
+                const objectives = singleScenario.querySelector('.objectives p')
+                const victory_points = singleScenario.querySelector('.victory_points ul')
+                const special_rules = singleScenario.querySelector('.special_rules')
+                const image = singleScenario.querySelector('.layout img')
 
                 let random = getAvailableNum(data.scenarios.length, rolledScenarios)
                 rolledScenarios.push(random)
@@ -33,6 +41,57 @@ const generate = () => {
                 const scenario = data.scenarios[random]
 
                 title.innerText = scenario.name
+                outline.innerText = scenario.outline
+                layout.innerText = scenario.layout
+                starting_position.innerText = scenario.starting_position
+                priority.innerText = scenario.priority
+                objectives.innerText = scenario.objectives
+                image.src = './images/' + scenario.image 
+
+                victory_points.innerHTML = '';
+                scenario.victory_points.forEach(single => {
+                    let li = document.createElement("li");
+                    li.appendChild(document.createTextNode(single));
+                    victory_points.appendChild(li);
+                })
+
+                special_rules.innerHTML = '<h3>Special Rules</h3>';
+                if(scenario.special_rules[0] !== 'This scenario has no special rules'){
+                    special_rules.classList.remove('hidden')
+                    scenario.special_rules.forEach(single => {
+                        let p = document.createElement('p')
+                        let span = document.createElement('span')
+                        span.appendChild(document.createTextNode(`${single.name}: `))
+                        span.style.fontWeight = 600;
+                        p.appendChild(span)
+                        p.appendChild(document.createTextNode(single.description))
+                        special_rules.appendChild(p)
+                        if(single.table){
+                            let table = document.createElement('table')
+                            const row = table.insertRow();
+                            const cell1 = row.insertCell()
+                            cell1.appendChild(document.createTextNode('D6'))
+                            cell1.style.border = '1px solid black';
+                            const cell2 = row.insertCell()
+                            cell2.appendChild(document.createTextNode(('Result')))
+                            cell2.style.border = '1px solid black';
+
+                            for (let index = 0; index < single.table[0].length; index++) {
+                                const row = table.insertRow();
+                                const cell1 = row.insertCell()
+                                cell1.appendChild(document.createTextNode(single.table[0][index]))
+                                cell1.style.border = '1px solid black';
+                                const cell2 = row.insertCell()
+                                cell2.appendChild(document.createTextNode(single.table[1][index]))
+                                cell2.style.border = '1px solid black';
+                            }
+
+                            special_rules.appendChild(table)
+                        }
+                    })        
+                } else if(!special_rules.classList.contains('hidden')) {
+                    special_rules.classList.add('hidden')
+                }
             })
         })
         .catch(err => console.log(err));
@@ -50,3 +109,12 @@ const getAvailableNum = (cap, rolled) => {
     }
     return random
 }
+
+const expandScenario = (element) => {
+    if(element.classList.contains('expanded')){
+        element.classList.remove('expanded')
+    } else {
+        element.classList.add('expanded')
+    }
+}
+    
